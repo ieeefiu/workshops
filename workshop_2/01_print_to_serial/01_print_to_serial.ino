3081bb0dfba742c19c9aa8659e441a30760985d5
William Oberndorfer <will.oberndorfer@gmail.com>
2016-09-22 14:02:13 -0400

    fix button problem

--- workshop_2/01_print_to_serial/01_print_to_serial.ino
+++ workshop_2/01_print_to_serial/01_print_to_serial.ino
@@ -1,8 +1,10 @@
 #define BUTTON_PIN 2
 
 void set_print();
+void count_presses();
 
-volatile enum { IDLE, PRINT } print_state = IDLE;
+volatile enum states { IDLE, PRINT } print_state = IDLE;
+volatile enum states previous_state = IDLE;
 
 int counter = 0;
 
@@ -13,17 +15,32 @@ void setup() {
 }
 
 void loop() {
-    if (print_state = PRINT) {
+    if (previous_state != print_state) {
+        count_presses();
+    }
+}
+
+void count_presses() {
+    switch(print_state) {
+    case IDLE:
+        break;
+    case PRINT:
         counter++;
         Serial.print("You have pressed the button ");
         Serial.print(counter);
         Serial.println(" times.");
-        print_state = IDLE;
+        delay(100);
+        break;
+    default:
+        break;
     }
+
+    previous_state = print_state;
 }
 
 void set_print() {
-    if (digitalRead(BUTTON_PIN)) {
+    int button = digitalRead(BUTTON_PIN);
+    if (button) {
         print_state = PRINT;        
     }
     else {
