9526abc56daf8bea82d527d643723ee61d0928d2
William Oberndorfer <will.oberndorfer@gmail.com>
2016-09-29 08:11:46 -0400

    add color sensor example. maybe it works, idk

---
+++ workshop_3/01_color_sensor/01_color_sensor.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+uint16_t color_data[4];
+
+void print_color_data(uint16_t *data);
+
+void setup() {
+    Serial.begin(9600);
+
+    if (tcs.begin()) {
+        Serial.println("Sensor initialized");
+    }
+    else {
+        Serial.println("Sensor not found");
+        while (1);
+    }
+}
+
+void loop() {
+    tcs.getRawData(&color_data[0],
+                   &color_data[1],
+                   &color_data[2],
+                   &color_data[3]);
+    print_color_data(color_data);
+}
+
+void print_color_data(uint16_t *data) {
+    Serial.print("R: ");
+    Serial.print(data[0], DEC);
+    Serial.print("G: ");
+    Serial.print(data[1], DEC);
+    Serial.print("B: ");
+    Serial.print(data[2], DEC);
+    Serial.print("C: ");
+    Serial.print(data[3], DEC);
+    Serial.println("");
+}
