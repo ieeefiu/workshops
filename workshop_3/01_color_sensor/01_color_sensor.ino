58883d450a34c88b550ed901536101603502a2cd
William Oberndorfer <will.oberndorfer@gmail.com>
2016-09-29 09:14:31 -0400

    add delay to sensor reading

--- workshop_3/01_color_sensor/01_color_sensor.ino
+++ workshop_3/01_color_sensor/01_color_sensor.ino
@@ -7,33 +7,34 @@ uint16_t color_data[4];
 void print_color_data(uint16_t *data);
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    if (tcs.begin()) {
-        Serial.println("Sensor initialized");
-    }
-    else {
-        Serial.println("Sensor not found");
-        while (1);
-    }
+  if (tcs.begin()) {
+    Serial.println("Sensor initialized");
+  }
+  else {
+    Serial.println("Sensor not found");
+    while (1);
+  }
 }
 
 void loop() {
-    tcs.getRawData(&color_data[0],
-                   &color_data[1],
-                   &color_data[2],
-                   &color_data[3]);
-    print_color_data(color_data);
+  tcs.getRawData(&color_data[0], &color_data[1], &color_data[2], &color_data[3]);
+  print_color_data(color_data);
+  delay(1000);
 }
 
 void print_color_data(uint16_t *data) {
-    Serial.print("R: ");
-    Serial.print(data[0], DEC);
-    Serial.print("G: ");
-    Serial.print(data[1], DEC);
-    Serial.print("B: ");
-    Serial.print(data[2], DEC);
-    Serial.print("C: ");
-    Serial.print(data[3], DEC);
-    Serial.println("");
+  Serial.print("R: ");
+  Serial.print(data[0], DEC);
+  Serial.print(" ");
+  Serial.print("G: ");
+  Serial.print(data[1], DEC);
+  Serial.print(" ");
+  Serial.print("B: ");
+  Serial.print(data[2], DEC);
+  Serial.print(" ");
+  Serial.print("C: ");
+  Serial.print(data[3], DEC);
+  Serial.println(" ");
 }
